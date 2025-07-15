ecedbd9d1057eb31ae2b863531d219bb8871cae1
shrestha.ayub09 <shrestha.ayub09@gmail.com>
2025-07-15 14:49:43 +0800

    changes commited

--- src/components/galleryItem/galleryItem.jsx
+++ src/components/galleryItem/galleryItem.jsx
@@ -1,9 +1,21 @@
 import './galleryItem.css'
 import {Link} from "react-router"
+import { IKImage } from 'imagekitio-react';
+
 const GalleryItem = ({item}) => {
     return (
         <div className="galleryItem" style={{gridRowEnd: `span ${Math.ceil(item.height/100)}`}}>
-            <img src={item.media} alt="" />
+            {/* <img src={item.media} alt="" /> */}
+            <IKImage
+                urlEndpoint={import.meta.env.VITE_URL_IK_ENDPOINT}
+                path={item.media}
+                transformation={[
+                    {
+                        height:200,
+                        width:200
+                    }
+                ]}
+            />
             <Link to={`/pin/${item.id}`} className="overlay"/>
             <button className='saveButton'>Save</button>
             <div className='overlayIcons'>
