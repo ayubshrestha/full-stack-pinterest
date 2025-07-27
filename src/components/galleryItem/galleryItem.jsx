112c3cec9536babb93d3753843a7b0b1fa4554c1
shrestha.ayub09 <shrestha.ayub09@gmail.com>
2025-07-27 15:00:51 +0800

    Post Detail Page added

--- src/components/galleryItem/galleryItem.jsx
+++ src/components/galleryItem/galleryItem.jsx
@@ -1,21 +1,13 @@
 import './galleryItem.css'
 import {Link} from "react-router"
-import { IKImage } from 'imagekitio-react';
+import Image from '../image/image'
 
 const GalleryItem = ({item}) => {
+
+    const optimizedHeight = (373 * item.height) / item.width
     return (
         <div className="galleryItem" style={{gridRowEnd: `span ${Math.ceil(item.height/100)}`}}>
-            {/* <img src={item.media} alt="" /> */}
-            <IKImage
-                urlEndpoint={import.meta.env.VITE_URL_IK_ENDPOINT}
-                path={item.media}
-                transformation={[
-                    {
-                        height:200,
-                        width:200
-                    }
-                ]}
-            />
+            <Image path={item.media} alt="" w={372} h={optimizedHeight}/>
             <Link to={`/pin/${item.id}`} className="overlay"/>
             <button className='saveButton'>Save</button>
             <div className='overlayIcons'>
--- src/components/leftBar/leftBar.jsx
+++ src/components/leftBar/leftBar.jsx
@@ -1,23 +1,23 @@
 import './leftBar.css'
-
+import Image from '../image/image'
 const LeftBar = () => {
     return (
         <div className='leftBar'>
             <div className='menuIcons'>
                 <a className='menuIcon' href="/">
-                    <img src="/general/logo.png" alt="logo" className='logo'/>
+                    <Image path="/general/logo.png" alt="logo" className='logo'/>
                 </a>
                 <a className='menuIcon' href="/">
-                    <img src="/general/home.svg" alt="home" />
+                    <Image path="/general/home.svg" alt="home" />
                 </a>
                 <a className='menuIcon' href="/">
-                    <img src="/general/create.svg" alt="create" />
+                    <Image path="/general/create.svg" alt="create" />
                 </a>
                 <a className='menuIcon' href="/">
-                    <img src="/general/updates.svg" alt="updates" />
+                    <Image path="/general/updates.svg" alt="updates" />
                 </a>
                 <a className='menuIcon' href="/">
-                    <img src="/general/messages.svg" alt="message" />
+                    <Image path="/general/messages.svg" alt="message" />
                 </a>
             </div>
             <a className='menuIcon' href="/">
